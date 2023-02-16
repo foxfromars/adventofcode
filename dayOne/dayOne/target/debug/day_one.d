951887bb525b32398511e57806ea3a95fed6b004
luis felipe <luisfeliperaposobatista@gmai.com>
2023-02-16 09:31:30 -0300

    new File organization

---
+++ dayOne/dayOne/target/debug/day_one.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/luis.raposo/Projects/day_one/target/debug/day_one: /mnt/c/Users/luis.raposo/Projects/day_one/src/main.rs
---
+++ dayOne/dayOne/target/debug/deps/day_one-e11cc76a82d10fcd.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/luis.raposo/Projects/day_one/target/debug/deps/day_one-e11cc76a82d10fcd: src/main.rs
+
+/mnt/c/Users/luis.raposo/Projects/day_one/target/debug/deps/day_one-e11cc76a82d10fcd.d: src/main.rs
+
+src/main.rs:
