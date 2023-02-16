f7247e3cf1974a513be7e980fe17189b572cd56d
luis felipe <luisfeliperaposobatista@gmai.com>
2023-02-16 09:39:00 -0300

    removed duplicated files

--- dayOne/dayOne/target/debug/day_one.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/Users/luis.raposo/Projects/day_one/target/debug/day_one: /mnt/c/Users/luis.raposo/Projects/day_one/src/main.rs
--- dayOne/dayOne/target/debug/deps/day_one-e11cc76a82d10fcd.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/luis.raposo/Projects/day_one/target/debug/deps/day_one-e11cc76a82d10fcd: src/main.rs
-
-/mnt/c/Users/luis.raposo/Projects/day_one/target/debug/deps/day_one-e11cc76a82d10fcd.d: src/main.rs
-
-src/main.rs:
