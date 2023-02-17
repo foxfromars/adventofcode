53453897987897a0d2ffae74f7efb7609e2dd09b
luis felipe <luisfeliperaposobatista@gmai.com>
2023-02-17 17:01:42 -0300

    day 2 part 1 finished

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,5 +1,4 @@
 use std::fs;
-use std::vec::IntoIter;
 
 fn main() {
     // A = ROCK
@@ -11,52 +10,70 @@ fn main() {
     // Z = SCISSORS
 
     fn result(play1: char, play2: char) -> i32 {
+        let mut score: i32 = 0;
+        if play2 == 'X' {
+            score += 1
+        }
+
+        if play2 == 'Y' {
+            score += 2
+        }
+
+        if play2 == 'Z' {
+            score += 3
+        }
+
         if play1 == 'A' {
             if play2 == 'X' {
-                return 6 as i32;
+                score += 3 as i32;
             } else if play2 == 'Y' {
-                return 3 as i32;
+                score += 6 as i32;
             } else {
-                return 8 as i32;
+                score += 0 as i32;
             }
-        }
-
-        else if play1 == 'B' {
+        } else if play1 == 'B' {
             if play2 == 'X' {
-                return 8 as i32;
+                score += 0 as i32;
             } else if play2 == 'Y' {
-                return 6 as i32;
+                score += 3 as i32;
             } else {
-                return 3 as i32;
+                score += 6 as i32;
             }
-        }
-
-        else {
+        } else {
             if play2 == 'X' {
-                return 3 as i32;
+                score += 6 as i32;
             } else if play2 == 'Y' {
-                return 8 as i32;
+                score += 0 as i32;
             } else {
-                return 6 as i32;
+                score += 3 as i32;
             }
         }
+        return score;
     }
-   
-    fn process_data (path: &str) -> Vec<String> {
-        let file_string = fs::read_to_string(path)
-            .expect("file should be read by the function");
+
+    fn process_data(path: &str) -> Vec<String> {
+        let file_string = fs::read_to_string(path).expect("file should be read by the function");
         // let plays: Vec<String> = file_string.split("\n").map(|s| s.to_owned()).collect();
         let temp_plays: Vec<&str> = file_string.split("\n").collect();
         let mut plays: Vec<String> = vec![];
-        
+
         for i in temp_plays {
             plays.push(i.split(" ").map(|s| s.to_owned()).collect());
         }
         return plays;
     }
 
-    let plays: Vec<String> = process_data("./assets/input");
+    let plays: Vec<String> = process_data("./src/assets/input");
+    let mut score: i32 = 0;
+
     for i in plays {
-        println!("{}", i);
+        //exception
+        if i == "" {
+            break;
+        }
+
+        let k: i32 = result(i.chars().nth(0).unwrap(), i.chars().nth(1).unwrap());
+        score += k;
     }
+    println!("{}", score);
 }
