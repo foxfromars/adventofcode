40ac7c782629639166373b78ccf9663698a603fd
luisfeliperaposobatista <luisfeliperaposobatista@gmail.com>
2023-02-19 19:44:16 -0300

    day_2-partTwo completed

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,6 +1,7 @@
 use std::fs;
 
 fn main() {
+    //PART ONE MEANINGs
     // A = ROCK
     // B = PAPER
     // C = SCISSORS
@@ -9,18 +10,23 @@ fn main() {
     // Y = PAPER
     // Z = SCISSORS
 
-    fn result(play1: char, play2: char) -> i32 {
+    //PART TWO MEANINGs
+    //X = LOSE
+    //Y = DRAW
+    //Z = WIN
+
+    fn result_part_one(play1: char, play2: char) -> i32 {
         let mut score: i32 = 0;
         if play2 == 'X' {
-            score += 1
+            score += 1;
         }
 
         if play2 == 'Y' {
-            score += 2
+            score += 2;
         }
 
         if play2 == 'Z' {
-            score += 3
+            score += 3;
         }
 
         if play1 == 'A' {
@@ -51,6 +57,43 @@ fn main() {
         return score;
     }
 
+    fn result_part_two(play1: char, result: char) -> i32 {
+        let mut score: i32 = 0;
+        if result == 'X' {
+            score += 0;
+            if play1 == 'A' {
+                score += 3;
+            } else if play1 == 'B' {
+                score += 1;
+            } else {
+                score += 2;
+            }
+        } else if result == 'Y' {
+            score += 3;
+            if play1 == 'A' {
+                score += 1;
+            }
+            else if play1 == 'B' {
+                score += 2;
+            }
+            else {
+                score += 3;
+            }
+        } else {
+            score += 6;
+            if play1 == 'A' {
+                score += 2;
+            }
+            else if play1 == 'B' {
+                score += 3;
+            }
+            else {
+                score += 1;
+            }
+        }
+        return score;
+    }
+
     fn process_data(path: &str) -> Vec<String> {
         let file_string = fs::read_to_string(path).expect("file should be read by the function");
         // let plays: Vec<String> = file_string.split("\n").map(|s| s.to_owned()).collect();
@@ -64,7 +107,8 @@ fn main() {
     }
 
     let plays: Vec<String> = process_data("./src/assets/input");
-    let mut score: i32 = 0;
+    let mut score_partone: i32 = 0;
+    let mut score_parttwo: i32 = 0;
 
     for i in plays {
         //exception
@@ -72,8 +116,13 @@ fn main() {
             break;
         }
 
-        let k: i32 = result(i.chars().nth(0).unwrap(), i.chars().nth(1).unwrap());
-        score += k;
+        let k: i32 = result_part_one(i.chars().nth(0).unwrap(), i.chars().nth(1).unwrap());
+        score_partone += k;
+
+        let j: i32 = result_part_two(i.chars().nth(0).unwrap(), i.chars().nth(1).unwrap());
+        score_parttwo += j;
     }
-    println!("{}", score);
+
+    println!("{}", score_partone);
+    println!("{}", score_parttwo);
 }
--- day_2/target/debug/day_2.d
+++ day_2/target/debug/day_2.d
@@ -1,1 +1,1 @@
-/mnt/c/Users/luis.raposo/Projects/day_one/day_2/target/debug/day_2: /mnt/c/Users/luis.raposo/Projects/day_one/day_2/src/main.rs
+/home/fox/Projects/adventofcode2022/day_2/target/debug/day_2: /home/fox/Projects/adventofcode2022/day_2/src/main.rs
